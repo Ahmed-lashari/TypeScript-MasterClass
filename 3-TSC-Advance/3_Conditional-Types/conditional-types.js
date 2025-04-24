205181e69d6d737aef19c879c5ce6528b2391df9
Ahmed-lashari <ahmedlashari.official@gmail.com>
2025-04-24 16:07:47 +0500

    Advance: Conditional Types explained

---
+++ 3-TSC-Advance/3_Conditional-Types/conditional-types.js
@@ -0,0 +1,12 @@
+/*
+ Conditional Types in TypeScript:
+
+ 1. Conditional Types allow you to create " types " based on a condition
+ 2. The syntax is similar to ternary operators in JavaScript:
+
+    A extends B ? C : D
+
+ 3. This can be read as: " If A can be assigned to B, then the type is C, otherwise D "
+
+ 4. This is very useful when working with generics and advanced type transformations
+ */
